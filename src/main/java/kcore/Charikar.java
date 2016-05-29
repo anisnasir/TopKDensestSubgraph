f5763660dc83101e06dcc9e2ada706b730a60f8b
Anis Nasir <anis.uddin@hotmail.com>
2016-05-29 08:26:34 +0200

    latest updates

--- src/main/java/kcore/Charikar.java
+++ src/main/java/kcore/Charikar.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Charikar {
+	
 	Charikar() {
 	}
 	ArrayList<String> getDensest(DegreeMap degreeMap,NodeMap nodeMap) {
-		int numEdges = nodeMap.numEdges;
 		int numNodes = nodeMap.getNumNodes();
+		int numEdges = nodeMap.numEdges;
+		
 		double density = numEdges/(double)numNodes;
-
-		ArrayList<String> densest = new ArrayList<String>(nodeMap.map.keySet());
-
+		ArrayList<String> densest = new ArrayList<String>();
+		
 		int i = 0 ;
 		while(i < degreeMap.capacity){
 			HashSet<String> temp = degreeMap.map.get(i);
-
 			if(temp.size() == 0) {
 				i++;
 			}
@@ -27,40 +27,40 @@ public class Charikar {
 					break;
 				}
 				temp.remove(element);
+				densest.add(element);
 				//System.out.println(element + " " + nodeMap.getDegree(element));
+				
 				HashSet<String> neighbors;
-
+				
 				if (nodeMap.getNeighbors(element) == null)
 					neighbors = new HashSet<String>();
 				else 
 					neighbors = new HashSet<String>(nodeMap.getNeighbors(element));
-
+				
 				for(String neighbor:neighbors) {
 					//System.out.println(element+ " " + neighbor);
 					nodeMap.removeNode(element, neighbor);
 					nodeMap.removeNode(neighbor, element);
 					numEdges--;
-
+					
 					int nodeDegree = nodeMap.getDegree(neighbor);
 					degreeMap.decremnetDegree(nodeDegree+1, neighbor);
 					if(nodeDegree < i) {
 						i=nodeDegree;
-					}	
+					}		
 				}
 				numNodes--;
-			}
-			if(numNodes == 0 ) {
-				System.out.println(density);
-				return densest;
-			}
-			double newDensity = numEdges/(double)numNodes;
-			if(density < newDensity) {
-				density = newDensity;
-				densest = new ArrayList<String>(nodeMap.map.keySet());
-			}
+				
+				double newDensity = numEdges/(double)numNodes;
+				if(newDensity >density) {
+					density = newDensity;
+					densest = new ArrayList<String>();
+				}
+				
+			}	
 		}
-		System.out.println(density);
+		System.out.println("main core " + density);
 		return densest;
-
+		
 	}
 }
