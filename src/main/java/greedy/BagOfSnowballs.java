111dd8046ddfab8b424e13dead436f665298cc0c
Anis Nasir <anis.uddin@hotmail.com>
2016-06-11 04:22:49 +0300

    final code

--- src/main/java/greedy/BagOfSnowballs.java
+++ src/main/java/greedy/BagOfSnowballs.java
@@ -122,7 +122,7 @@ public class BagOfSnowballs implements DensestSubgraph{
 		//System.out.println(src + " " + srcDegree + " " + dst + "  " + dstDegree + " " + nodeMap.getNeighbors(dst)); 
 
 		double initialDensity= this.getMaximalDensity(nodeMap);
-		
+
 		SnowBall temp = null;
 		if (srcDegree < maximalDensity && dstDegree < maximalDensity) {
 			return;
@@ -144,13 +144,13 @@ public class BagOfSnowballs implements DensestSubgraph{
 				this.fixMainCore(src, nodeMap, srcSnowBall);
 			}
 			SnowBall dstSnowBall = addNode(dst, nodeMap);
-			
+
 			if(!this.verifyMainCore(dst, nodeMap,dstSnowBall)) {
 				this.fixMainCore(dst, nodeMap, dstSnowBall);
 			}
-			
+
 			addEdgeKCore(edge);
-			
+
 			if(srcSnowBall.equals(dstSnowBall)) {
 				srcSnowBall.addEdge(edge);
 				ensureInvariant(srcSnowBall,nodeMap);	
@@ -330,23 +330,23 @@ public class BagOfSnowballs implements DensestSubgraph{
 		if(prevSrcDegree < maximalDensity && prevDstDegree < maximalDensity)
 			return;
 
-			double currentMaximalDensity = this.getMaximalDensity(nodeMap);
-			boolean flag = false;
+		double currentMaximalDensity = this.getMaximalDensity(nodeMap);
+		boolean flag = false;
 
-			if(prevSrcDegree >= maximalDensity ) {
-				if(srcDegree < maximalDensity || srcDegree == 0) {
-					//SnowBall temp = null;
-					for(SnowBall s: bag) {
-						if(s.containsNode(src)) {
-							s.removeNode(src);
-							//stats.removeNode(s, src);
-							ensureInvariant(s,nodeMap);
-							//temp = s;
-							flag = true;
-							break;
-						}	
-					}
-					/*if(temp !=null) {
+		if(prevSrcDegree >= maximalDensity ) {
+			if(srcDegree < maximalDensity || srcDegree == 0) {
+				//SnowBall temp = null;
+				for(SnowBall s: bag) {
+					if(s.containsNode(src)) {
+						s.removeNode(src);
+						//stats.removeNode(s, src);
+						ensureInvariant(s,nodeMap);
+						//temp = s;
+						flag = true;
+						break;
+					}	
+				}
+				/*if(temp !=null) {
 						ArrayList<String> nodes = new ArrayList<String>(temp.getNodes());
 						bag.remove(temp);
 						//stats.removeSnowBall(temp);
@@ -354,232 +354,232 @@ public class BagOfSnowballs implements DensestSubgraph{
 							this.addNode(node, nodeMap);
 						}
 					}*/
-					this.removeNodekCore(src);
-				}
+				this.removeNodekCore(src);
 			}
-			if( prevDstDegree >= maximalDensity) {
-				if(dstDegree < maximalDensity || dstDegree == 0) {
-					//SnowBall temp = null;
-					for(SnowBall s: bag) {
+		}
+		if( prevDstDegree >= maximalDensity) {
+			if(dstDegree < maximalDensity || dstDegree == 0) {
+				//SnowBall temp = null;
+				for(SnowBall s: bag) {
 
-						if(s.containsNode(dst)) {
-							s.removeNode(dst);
-							//stats.removeNode(s, dst);
-							//System.out.println(s.getNodes());
-							ensureInvariant(s,nodeMap);
-							//System.out.println(s.getNodes());
-							
-							//temp = s;
+					if(s.containsNode(dst)) {
+						s.removeNode(dst);
+						//stats.removeNode(s, dst);
+						//System.out.println(s.getNodes());
+						ensureInvariant(s,nodeMap);
+						//System.out.println(s.getNodes());
 
-							flag = true;
-							break;
-						}	
-					}
-					/*if(temp !=null) {
+						//temp = s;
+
+						flag = true;
+						break;
+					}	
+				}
+				/*if(temp !=null) {
 
 						ArrayList<String> nodes = new ArrayList<String>(temp.getNodes());
 						bag.remove(temp);
 						//stats.removeSnowBall(temp);
 						for(String node: nodes) { 
 							SnowBall sTemp = this.addNode(node, nodeMap);
-							
+
 						}
 					}*/
-					this.removeNodekCore(dst);
-				}
-			} 
+				this.removeNodekCore(dst);
+			}
+		} 
 
-			if(prevSrcDegree < maximalDensity || prevDstDegree < maximalDensity) {
+		if(prevSrcDegree < maximalDensity || prevDstDegree < maximalDensity) {
+			return;
+		}
+
+		if(!flag) {
+			this.removeEdgeKCore(edge);
+			SnowBall temp = null;
+			for(int i =0 ;i< bag.size();i++) {
+				SnowBall s = bag.get(i);
+				if(s.containsEdge(edge)){
+					s.removeEdge(edge,nodeMap);
+					temp =s;
+					ensureInvariant(s,nodeMap);
+				}
+
+			}
+
+			if(temp!=null) {
+				if(temp.contains(src) && temp.contains(dst)) {
+					ArrayList<String> visited = new ArrayList<String>();
+					//checking for disconnected snowBalls
+					if(!isConnected(temp,src,dst,visited)) {
+						SnowBall newSnowBall = new SnowBall();
+						for(String s:visited) {
+							temp.removeNode(s);
+							//stats.removeNode(temp, s);
+							if(nodeMap.getDegree(s) >= maximalDensity) {
+								newSnowBall.addNode(s, nodeMap);
+								//this.updateStats(newSnowBall, s, nodeMap);
+							}
+						}
+						ensureInvariant(newSnowBall,nodeMap);
+						ensureInvariant(temp,nodeMap);
+						if(!newSnowBall.isEmpty())
+							bag.add(newSnowBall);
+					}
+				}
+			}
+		}
+
+		if(currentMaximalDensity != this.getMaximalDensity(nodeMap)) {
+			synchronizeSnowBalls(nodeMap);
+		}
+		cleanup(nodeMap);
+
+		//System.out.println("-bag graph" + bagGraph+ " " + maximalDensity);
+		//this.print();
+
+		double updatedMaximalDensity = maximalDensity;
+		if(currentMaximalDensity != updatedMaximalDensity) {
+			HashSet<String> addNodes = degreeMap.getNodesBetween(currentMaximalDensity, updatedMaximalDensity);
+			if(addNodes == null){
 				return;
 			}
 
-			if(!flag) {
-				this.removeEdgeKCore(edge);
-				SnowBall temp = null;
-				for(int i =0 ;i< bag.size();i++) {
-					SnowBall s = bag.get(i);
-					if(s.containsEdge(edge)){
-						s.removeEdge(edge,nodeMap);
-						temp =s;
-						ensureInvariant(s,nodeMap);
-					}
-
-				}
-
-				if(temp!=null) {
-					if(temp.contains(src) && temp.contains(dst)) {
-						ArrayList<String> visited = new ArrayList<String>();
-						//checking for disconnected snowBalls
-						if(!isConnected(temp,src,dst,visited)) {
-							SnowBall newSnowBall = new SnowBall();
-							for(String s:visited) {
-								temp.removeNode(s);
-								//stats.removeNode(temp, s);
-								if(nodeMap.getDegree(s) >= maximalDensity) {
-									newSnowBall.addNode(s, nodeMap);
-									//this.updateStats(newSnowBall, s, nodeMap);
-								}
-							}
-							ensureInvariant(newSnowBall,nodeMap);
-							ensureInvariant(temp,nodeMap);
-							if(!newSnowBall.isEmpty())
-								bag.add(newSnowBall);
-						}
-					}
-				}
-			}
-
-			if(currentMaximalDensity != this.getMaximalDensity(nodeMap)) {
-				synchronizeSnowBalls(nodeMap);
-			}
-			cleanup(nodeMap);
-
-			//System.out.println("-bag graph" + bagGraph+ " " + maximalDensity);
-			//this.print();
-			
-			double updatedMaximalDensity = maximalDensity;
-			if(currentMaximalDensity != updatedMaximalDensity) {
-				HashSet<String> addNodes = degreeMap.getNodesBetween(currentMaximalDensity, updatedMaximalDensity);
-				if(addNodes == null){
-					return;
-				}
-
-				for(String str: addNodes) {
-					addNode(str,nodeMap);
-				}
-			}
-
-
-			if(this.getMaximalDensity(nodeMap) != updatedMaximalDensity) {
-				synchronizeSnowBalls(nodeMap);
-			}
-			cleanup(nodeMap);
-
-		}
-
-		void ensureInvariant(SnowBall s, NodeMap nodeMap) {
-			HashSet<String> nodes = new HashSet<String>();
-			s.ensureFirstInVariant(nodeMap,nodes,this);
-			for(String node: nodes) {
-				addNode(node,nodeMap);
-				//System.out.println("adding node "+ node);
+			for(String str: addNodes) {
+				addNode(str,nodeMap);
 			}
 		}
-		void synchronizeSnowBalls(NodeMap nodeMap) {
-			ArrayList<SnowBall> removable = new ArrayList<SnowBall> ();
-			for(int i = 0;i<bag.size();i++) {
-				SnowBall s = bag.get(i);
-				if(s.getDensity() < maximalDensity ) {
-					s.setMaximalDensity(maximalDensity, nodeMap);
-					ensureInvariant(s,nodeMap);	
-					if(s.isEmpty())
-						removable.add(s);
-				}
-			}
-			for(SnowBall s: removable) {
-				bag.remove(s);
-			}
+
+
+		if(this.getMaximalDensity(nodeMap) != updatedMaximalDensity) {
+			synchronizeSnowBalls(nodeMap);
 		}
-		boolean isConnected(SnowBall snowBall, String src, String dst, ArrayList<String> visited) {
-			//System.out.println(src+ " " + visited);
-			visited.add(src);
-			HashSet<String> neighbors = snowBall.graph.get(src);
-			if(neighbors == null)
-				return false;
-			if(neighbors.contains(dst))
-				return true;
-			for(String s:neighbors) {
-				if(!visited.contains(s))
-					if(isConnected(snowBall,s,dst,visited))
-						return true;
-			}
-			return false;
+		cleanup(nodeMap);
 
-		}
-		Densest getApproximation() {
-			Densest returnResults = new Densest();
-			double maxDensity = 0;
-			for(SnowBall s: bag) {
-				if(s.density > maxDensity) {
-					maxDensity = s.getDensity();
-					returnResults.setDensity(maxDensity);
-					returnResults.setDensest(s.graph);
-				}
-			}
-			return returnResults;
-		}
+	}
 
-		LinkedList<Densest> getTopK(int k) {
-			LinkedList<Densest> returnList = new LinkedList<Densest>();
-			Collections.sort(bag, Collections.reverseOrder());
-			int i =0;
-			while(i< k && i<bag.size()) {
-				SnowBall b = bag.get(i);
-				Densest d = new Densest();
-				d.setDensest(b.graph);
-				d.setDensity(b.getDensity());
-				returnList.add(d);
-				i++;
-			}
-			return returnList;
-		}
-
-		boolean verifyMainCore(String src, NodeMap nodeMap, SnowBall srcSnowBall) { 
-			if(!bagGraph.containsKey(src))
-				return true;
-
-			HashSet<String> visited = new HashSet<String>();
-			HashSet<String> neighbors = new HashSet<String>();
-
-			kCore.color(src, kCore.getKCore(src), visited, neighbors);
-
-			HashSet<String> visited1 = new HashSet<String>();
-			HashSet<String> neighbors1 = new HashSet<String>();
-
-			srcSnowBall.kCore.color(src, srcSnowBall.kCore.getKCore(src), visited1, neighbors1);
-
-			if(neighbors1.equals(neighbors))
-				return false;
-			else
-				return true;
-		}
-		void fixMainCore(String src, NodeMap nodeMap, SnowBall srcSnowBall) {
-			HashSet<String> visited = new HashSet<String>();
-			HashSet<String> neighbors = new HashSet<String>();
-			HashSet<SnowBall> remove = new HashSet<SnowBall>();
-
-			kCore.color(src, kCore.getKCore(src), visited, neighbors);
-			for(SnowBall s:bag) {			
-				if(s.intersection(s.getNodes(), neighbors) > 0  && !srcSnowBall.equals(s)) {
-					srcSnowBall.merge(s, nodeMap);
-					remove.add(s);
-				}
-			}
-			for(SnowBall s:remove) {
-				bag.remove(s);
-			}
-			this.ensureInvariant(srcSnowBall, nodeMap);
-		}
-
-		@Override
-		public ArrayList<Output> getDensest(DegreeMap degreeMap, NodeMap nodeMap) {
-			ArrayList<Output> outputArray = new ArrayList<Output>();
-			
-			PriorityQueue<SnowBall> queue = new PriorityQueue<SnowBall>(k,Collections.reverseOrder());
-			for(int i =0;i<bag.size();i++) {
-				SnowBall temp = bag.get(i);
-				queue.offer(temp);
-			}
-			
-			while(!queue.isEmpty()) {
-				SnowBall s = queue.poll();
-				Output returnOutput = new Output();
-				returnOutput.setCoreNum(s.getMainCore());
-				returnOutput.setDensity(s.getDensity());
-				returnOutput.setNodes(new ArrayList<String>(s.getNodes()));
-				returnOutput.setSize(s.getNumNodes());
-				outputArray.add(returnOutput);
-			}
-			return outputArray;
+	void ensureInvariant(SnowBall s, NodeMap nodeMap) {
+		HashSet<String> nodes = new HashSet<String>();
+		s.ensureFirstInVariant(nodeMap,nodes,this);
+		for(String node: nodes) {
+			addNode(node,nodeMap);
+			//System.out.println("adding node "+ node);
 		}
 	}
+	void synchronizeSnowBalls(NodeMap nodeMap) {
+		ArrayList<SnowBall> removable = new ArrayList<SnowBall> ();
+		for(int i = 0;i<bag.size();i++) {
+			SnowBall s = bag.get(i);
+			if(s.getDensity() < maximalDensity ) {
+				s.setMaximalDensity(maximalDensity, nodeMap);
+				ensureInvariant(s,nodeMap);	
+				if(s.isEmpty())
+					removable.add(s);
+			}
+		}
+		for(SnowBall s: removable) {
+			bag.remove(s);
+		}
+	}
+	boolean isConnected(SnowBall snowBall, String src, String dst, ArrayList<String> visited) {
+		//System.out.println(src+ " " + visited);
+		visited.add(src);
+		HashSet<String> neighbors = snowBall.graph.get(src);
+		if(neighbors == null)
+			return false;
+		if(neighbors.contains(dst))
+			return true;
+		for(String s:neighbors) {
+			if(!visited.contains(s))
+				if(isConnected(snowBall,s,dst,visited))
+					return true;
+		}
+		return false;
+
+	}
+	Densest getApproximation() {
+		Densest returnResults = new Densest();
+		double maxDensity = 0;
+		for(SnowBall s: bag) {
+			if(s.density > maxDensity) {
+				maxDensity = s.getDensity();
+				returnResults.setDensity(maxDensity);
+				returnResults.setDensest(s.graph);
+			}
+		}
+		return returnResults;
+	}
+
+	LinkedList<Densest> getTopK(int k) {
+		LinkedList<Densest> returnList = new LinkedList<Densest>();
+		Collections.sort(bag, Collections.reverseOrder());
+		int i =0;
+		while(i< k && i<bag.size()) {
+			SnowBall b = bag.get(i);
+			Densest d = new Densest();
+			d.setDensest(b.graph);
+			d.setDensity(b.getDensity());
+			returnList.add(d);
+			i++;
+		}
+		return returnList;
+	}
+
+	boolean verifyMainCore(String src, NodeMap nodeMap, SnowBall srcSnowBall) { 
+		if(!bagGraph.containsKey(src))
+			return true;
+
+		HashSet<String> visited = new HashSet<String>();
+		HashSet<String> neighbors = new HashSet<String>();
+
+		kCore.color(src, kCore.getKCore(src), visited, neighbors);
+
+		HashSet<String> visited1 = new HashSet<String>();
+		HashSet<String> neighbors1 = new HashSet<String>();
+
+		srcSnowBall.kCore.color(src, srcSnowBall.kCore.getKCore(src), visited1, neighbors1);
+
+		if(neighbors1.equals(neighbors))
+			return false;
+		else
+			return true;
+	}
+	void fixMainCore(String src, NodeMap nodeMap, SnowBall srcSnowBall) {
+		HashSet<String> visited = new HashSet<String>();
+		HashSet<String> neighbors = new HashSet<String>();
+		HashSet<SnowBall> remove = new HashSet<SnowBall>();
+
+		kCore.color(src, kCore.getKCore(src), visited, neighbors);
+		for(SnowBall s:bag) {			
+			if(s.intersection(s.getNodes(), neighbors) > 0  && !srcSnowBall.equals(s)) {
+				srcSnowBall.merge(s, nodeMap);
+				remove.add(s);
+			}
+		}
+		for(SnowBall s:remove) {
+			bag.remove(s);
+		}
+		this.ensureInvariant(srcSnowBall, nodeMap);
+	}
+
+	@Override
+	public ArrayList<Output> getDensest(DegreeMap degreeMap, NodeMap nodeMap) {
+		ArrayList<Output> outputArray = new ArrayList<Output>();
+
+		PriorityQueue<SnowBall> queue = new PriorityQueue<SnowBall>(k,Collections.reverseOrder());
+		for(int i =0;i<bag.size();i++) {
+			SnowBall temp = bag.get(i);
+			queue.offer(temp);
+		}
+
+		while(!queue.isEmpty()) {
+			SnowBall s = queue.poll();
+			Output returnOutput = new Output();
+			returnOutput.setCoreNum(s.getMainCore());
+			returnOutput.setDensity(s.getDensity());
+			returnOutput.setNodes(new ArrayList<String>(s.getNodes()));
+			returnOutput.setSize(s.getNumNodes());
+			outputArray.add(returnOutput);
+		}
+		return outputArray;
+	}
+}
