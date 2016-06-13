cdcdc414c10e4ec7dbae137ce89bb7751cf73c6c
Anis Uddin <dradnan89@hotmail.com>
2016-06-13 13:09:04 +0200

    added run simulation

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 JAR="target/kcore-0.0.1-SNAPSHOT.jar"
-inFile="/home/anis/Datasets/soc-LiveJournal1.txt"
-command="java -Xms1024m -Xmx8192m -jar ${JAR} 0 ${inFile} 0.005 "
+simulatorType=10
+inDir="/home/anis/Datasets/"
+inFile="com-amazon.ungraph"
+window=100000
+epsilon=0.05
+algo="greedy"
+k=10
+command="java -Xms1024m -Xmx8192m -jar ${JAR} ${simulatorType} ${inDir}${inFile}.txt ${window} ${epsilon} output_${algo}_${inFile}_${window}_${epsilon}_${k}.out ${k} output_logs/${algo}/k${k}/"
+
 
 $command
 
