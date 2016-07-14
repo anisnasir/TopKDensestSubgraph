9bf71ec06ff8d2fcd5947b38ef490f6cf2d7c5e7
Anis Nasir <anis.uddin@hotmail.com>
2016-07-15 03:37:59 +0500

    added

--- run-simulation.sh
+++ run-simulation.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#/bin/bash
 JAR="target/kcore-0.0.1-SNAPSHOT.jar"
 simulatorType=10
 inDir="/home/anis/Datasets/"
-inFile="com-amazon.ungraph"
-window=100000
-epsilon=0.05
+inFile="com-lj-1.5m.ungraph"
+window=1000001
+epsilon=0.1
 algo="greedy"
 k=10
-command="java -Xms1024m -Xmx8192m -jar ${JAR} ${simulatorType} ${inDir}${inFile}.txt ${window} ${epsilon} output_${algo}_${inFile}_${window}_${epsilon}_${k}.out ${k} output_logs/${algo}/k${k}/"
+command="java -d64 -Xms2g -Xmx16g -jar ${JAR} ${simulatorType} ${inDir}${inFile}.txt ${window} ${epsilon} output_${algo}_${inFile}_${window}_${epsilon}_${k}.out ${k} output_logs/${algo}/k${k}/"
 
 
 $command
-
